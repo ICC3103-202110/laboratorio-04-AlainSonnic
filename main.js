51a33c1ca674568408e2121bbc6aac62a9a508f5
alain <asonnic@miuandes.cl>
2021-05-06 14:50:43 -0400

    paso 1

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 /*empezando el lab*/
+
+/*
+const prompt = require('prompt-sync')();
+let name = prompt("Enter your name: ");
+console.log(`Hello ${name}`) */
+
+function View(counter){
+    return ('Count = '+counter);
+}
+
+
+console.log(View(51));
